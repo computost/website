593afd7ddfdd03a040fe27ed3d0c1016956f5ef7
Melody Universe <melody@computost.com>
2025-04-30 16:17:47 -0700

    remove snapshot test from layout

--- app/routes/_index.browser.test.tsx
+++ app/routes/_index.browser.test.tsx
@@ -1,89 +1,14 @@
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { expect, it } from "vitest";
 
 import Layout from "./_index";
 
 it("renders static content", () => {
-  const { container } = render(<Layout />, {
+  render(<Layout />, {
     wrapper: MemoryRouter,
   });
 
-  expect(container).toMatchInlineSnapshot(`
-    <div>
-      <header
-        class="flex justify-center gap-10 bg-orange-100 dark:bg-orange-950"
-      >
-        <a
-          data-discover="true"
-          href="/"
-        >
-          Computost Consulting
-        </a>
-        <nav
-          aria-label="Main"
-          data-orientation="horizontal"
-          dir="ltr"
-        >
-          <div
-            style="position: relative;"
-          >
-            <ul
-              data-orientation="horizontal"
-              dir="ltr"
-            >
-              <li>
-                <a
-                  data-discover="true"
-                  data-radix-collection-item=""
-                  href="/"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  data-discover="true"
-                  data-radix-collection-item=""
-                  href="/coop"
-                >
-                  Co-op Info
-                </a>
-              </li>
-              <li>
-                <a
-                  data-discover="true"
-                  data-radix-collection-item=""
-                  href="/services"
-                >
-                  Services
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <footer>
-        <p>
-          Contact us at
-           
-          <a
-            href="mailto:consulting@computost.com"
-          >
-            consulting@computost.com
-          </a>
-        </p>
-        <p>
-          Copyright © 
-          2025
-           Computost Consulting, LLC
-        </p>
-      </footer>
-    </div>
-  `);
-
-  const navigation = screen.getByRole("navigation");
-  expect(
-    within(navigation).getByRole("link", { name: "Home" }),
-  ).toBeInTheDocument();
+  expect(screen.getByRole("banner")).toBeInTheDocument();
+  expect(screen.getByRole("contentinfo")).toBeInTheDocument();
 });
