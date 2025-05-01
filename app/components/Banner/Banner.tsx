530f5e5824bbc1ae101b1c68ad5b615cf1d37b8f
Melody Universe <melody@computost.com>
2025-04-30 17:56:25 -0700

    shrink banner gap

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren, ReactNode } from "react";
 export function Banner({ children }: PropsWithChildren): ReactNode {
   return (
     <header className="mb-2 flex justify-center bg-orange-100 pt-2 pb-2 shadow-md shadow-black dark:bg-orange-950">
-      <div className="flex w-full max-w-7xl items-start gap-10">{children}</div>
+      <div className="flex w-full max-w-7xl items-start gap-4">{children}</div>
     </header>
   );
 }
