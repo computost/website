e7963999e9325ea293aed64a8bf7ffdca81817ee
Melody Universe <melody@computost.com>
2025-04-30 17:16:22 -0700

    pull banner into separate file

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -1,9 +1,1 @@
-import type { PropsWithChildren, ReactNode } from "react";
-
-export function Banner({ children }: PropsWithChildren): ReactNode {
-  return (
-    <header className="flex justify-center gap-10 bg-orange-100 dark:bg-orange-950">
-      {children}
-    </header>
-  );
-}
+export { Banner } from "./Banner";
