841655e2ef644cb30b6c006c07f72e148c86030b
Melody Universe <melody@computost.com>
2025-05-02 16:32:12 -0700

    increase padding for cards

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -2,6 +2,6 @@ import type { PropsWithChildren, ReactNode } from "react";
 
 export function Card({ children }: PropsWithChildren): ReactNode {
   return (
-    <article className="bg-green-50 p-2 dark:bg-green-950">{children}</article>
+    <article className="bg-green-50 p-4 dark:bg-green-950">{children}</article>
   );
 }
