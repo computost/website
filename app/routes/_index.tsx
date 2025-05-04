991607eb01d257b968728b1faa48103709db1f12
Melody Universe <melody@computost.com>
2025-05-04 14:07:08 -0700

    add padding to banner

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,7 @@ import {
 export default function Layout(): ReactNode {
   return (
     <>
-      <header className="flex justify-center bg-orange-100 pt-2 pb-2 shadow-md shadow-black dark:bg-orange-950">
+      <header className="flex justify-center bg-orange-100 p-2 pt-2 pb-2 shadow-md shadow-black dark:bg-orange-950">
         <div className="flex w-full max-w-7xl items-start gap-4">
           <ComputostLogo
             className="h-8 fill-stone-900 dark:fill-stone-100"
