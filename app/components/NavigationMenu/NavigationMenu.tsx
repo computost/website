37c54b4d0462a0c675d5f08e19348b0c1233b61f
Joshua Hogsett <joshua@computost.com>
2025-05-04 12:39:42 -0700

    nav menu dropdown

--- app/components/NavigationMenu/NavigationMenu.tsx
+++ app/components/NavigationMenu/NavigationMenu.tsx
@@ -1,16 +1,26 @@
 import type { ReactElement, ReactNode } from "react";
 
-import { NavigationMenu as RadixNavigationMenu } from "radix-ui";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { DropdownMenu, NavigationMenu as RadixNavigationMenu } from "radix-ui";
 
 import type { NavigationMenuItemProps } from "./NavigationMenuItem";
 
 export function NavigationMenu({ children }: NavigationMenuProps): ReactNode {
   return (
-    <RadixNavigationMenu.Root>
-      <RadixNavigationMenu.List className="flex gap-2">
-        {children}
-      </RadixNavigationMenu.List>
-    </RadixNavigationMenu.Root>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger aria-label="Open navigation" className="ml-auto">
+        <HamburgerMenuIcon className="size-6" />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content asChild>
+          <RadixNavigationMenu.Root orientation="vertical">
+            <RadixNavigationMenu.List className="flex flex-col gap-2">
+              {children}
+            </RadixNavigationMenu.List>
+          </RadixNavigationMenu.Root>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   );
 }
 
--- app/components/NavigationMenu/NavigationMenuItem.tsx
+++ app/components/NavigationMenu/NavigationMenuItem.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren, ReactNode } from "react";
 
-import { NavigationMenu as RadixNavigationMenu } from "radix-ui";
+import { DropdownMenu, NavigationMenu as RadixNavigationMenu } from "radix-ui";
 import { Link, useLocation } from "react-router";
 
 import { mergeClassNames } from "~/utils/mergeClassNames";
@@ -12,21 +12,23 @@ export function NavigationMenuItem({
   const { pathname } = useLocation();
 
   return (
-    <RadixNavigationMenu.Item>
-      <RadixNavigationMenu.Link asChild>
-        <Link
-          className={mergeClassNames(
-            "block bg-radial pt-1.5 pr-2 pb-1.5 pl-2 uppercase transition",
-            pathname === to
-              ? "from-orange-300 dark:from-orange-800"
-              : "hover:from-orange-200 focus:from-orange-200 dark:hover:from-orange-900 dark:focus:from-orange-900",
-          )}
-          to={to}
-        >
-          {children}
-        </Link>
-      </RadixNavigationMenu.Link>
-    </RadixNavigationMenu.Item>
+    <DropdownMenu.DropdownMenuItem asChild>
+      <RadixNavigationMenu.Item>
+        <RadixNavigationMenu.Link asChild>
+          <Link
+            className={mergeClassNames(
+              "block bg-radial pt-1.5 pr-2 pb-1.5 pl-2 uppercase transition",
+              pathname === to
+                ? "from-orange-300 dark:from-orange-800"
+                : "hover:from-orange-200 focus:from-orange-200 dark:hover:from-orange-900 dark:focus:from-orange-900",
+            )}
+            to={to}
+          >
+            {children}
+          </Link>
+        </RadixNavigationMenu.Link>
+      </RadixNavigationMenu.Item>
+    </DropdownMenu.DropdownMenuItem>
   );
 }
 
