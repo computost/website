155dbc1f617ab02faa1e92dd472b5048d1aa9a0f
Melody Universe <melody@computost.com>
2025-05-04 16:16:07 -0700

    conditionally use dropdown for nav menu

--- app/components/NavigationMenu/NavigationMenu.tsx
+++ app/components/NavigationMenu/NavigationMenu.tsx
@@ -3,24 +3,66 @@ import type { ReactElement, ReactNode } from "react";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { DropdownMenu, NavigationMenu as RadixNavigationMenu } from "radix-ui";
 
+import { assertUnreachable } from "~/utils/assertUnreachable";
+
 import type { NavigationMenuItemProps } from "./NavigationMenuItem";
+
+import {
+  NavigationMenuDisplayTypeProvider,
+  useNavigationMenuDisplayType,
+} from "./useNavigationMenuDisplayType";
 
 export function NavigationMenu({ children }: NavigationMenuProps): ReactNode {
   return (
+    <NavigationMenuDisplayTypeProvider>
+      <NavigationMenuCore>{children}</NavigationMenuCore>
+    </NavigationMenuDisplayTypeProvider>
+  );
+}
+
+function NavigationMenuCore({ children }: NavigationMenuProps): ReactNode {
+  const displayType = useNavigationMenuDisplayType();
+
+  switch (displayType) {
+    case "dropdown":
+      return <DropdownNavigationMenu>{children}</DropdownNavigationMenu>;
+    case "inline":
+      return <InlineNavigationMenu>{children}</InlineNavigationMenu>;
+    default:
+      assertUnreachable(displayType);
+  }
+}
+
+function DropdownNavigationMenu({ children }: NavigationMenuProps): ReactNode {
+  return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger aria-label="Open navigation" className="ml-auto">
+      <DropdownMenu.Trigger
+        aria-label="Open navigation"
+        className="ml-auto cursor-pointer"
+      >
         <HamburgerMenuIcon className="size-6" />
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content asChild>
-          <RadixNavigationMenu.Root orientation="vertical">
-            <RadixNavigationMenu.List className="flex flex-col gap-2">
-              {children}
-            </RadixNavigationMenu.List>
+          <RadixNavigationMenu.Root
+            className="rounded-md border border-stone-950 bg-stone-50 dark:border-stone-50 dark:bg-stone-950"
+            orientation="vertical"
+          >
+            <RadixNavigationMenu.List>{children}</RadixNavigationMenu.List>
           </RadixNavigationMenu.Root>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
+  );
+}
+
+function InlineNavigationMenu({ children }: NavigationMenuProps): ReactNode {
+  return (
+    <RadixNavigationMenu.Root>
+      <RadixNavigationMenu.List className="flex gap-2">
+        {children}
+      </RadixNavigationMenu.List>
+    </RadixNavigationMenu.Root>
   );
 }
 
--- app/components/NavigationMenu/NavigationMenuItem.tsx
+++ app/components/NavigationMenu/NavigationMenuItem.tsx
@@ -3,9 +3,34 @@ import type { PropsWithChildren, ReactNode } from "react";
 import { DropdownMenu, NavigationMenu as RadixNavigationMenu } from "radix-ui";
 import { Link, useLocation } from "react-router";
 
+import { assertUnreachable } from "~/utils/assertUnreachable";
 import { mergeClassNames } from "~/utils/mergeClassNames";
 
+import { useNavigationMenuDisplayType } from "./useNavigationMenuDisplayType";
+
 export function NavigationMenuItem({
+  children,
+  to,
+}: NavigationMenuItemProps): ReactNode {
+  const displayType = useNavigationMenuDisplayType();
+
+  switch (displayType) {
+    case "dropdown":
+      return (
+        <DropdownNavigationMenuItem to={to}>
+          {children}
+        </DropdownNavigationMenuItem>
+      );
+    case "inline":
+      return (
+        <InlineNavigationMenuItem to={to}>{children}</InlineNavigationMenuItem>
+      );
+    default:
+      assertUnreachable(displayType);
+  }
+}
+
+function DropdownNavigationMenuItem({
   children,
   to,
 }: NavigationMenuItemProps): ReactNode {
@@ -17,7 +42,7 @@ export function NavigationMenuItem({
         <RadixNavigationMenu.Link asChild>
           <Link
             className={mergeClassNames(
-              "block bg-radial pt-1.5 pr-2 pb-1.5 pl-2 uppercase transition",
+              "block bg-gradient-to-b pt-1.5 pr-2 pb-1.5 pl-2 uppercase transition",
               pathname === to
                 ? "from-orange-300 dark:from-orange-800"
                 : "hover:from-orange-200 focus:from-orange-200 dark:hover:from-orange-900 dark:focus:from-orange-900",
@@ -29,6 +54,31 @@ export function NavigationMenuItem({
         </RadixNavigationMenu.Link>
       </RadixNavigationMenu.Item>
     </DropdownMenu.DropdownMenuItem>
+  );
+}
+
+function InlineNavigationMenuItem({
+  children,
+  to,
+}: NavigationMenuItemProps): ReactNode {
+  const { pathname } = useLocation();
+
+  return (
+    <RadixNavigationMenu.Item>
+      <RadixNavigationMenu.Link asChild>
+        <Link
+          className={mergeClassNames(
+            "block bg-radial pt-1.5 pr-2 pb-1.5 pl-2 uppercase transition",
+            pathname === to
+              ? "from-orange-300 dark:from-orange-800"
+              : "hover:from-orange-200 focus:from-orange-200 dark:hover:from-orange-900 dark:focus:from-orange-900",
+          )}
+          to={to}
+        >
+          {children}
+        </Link>
+      </RadixNavigationMenu.Link>
+    </RadixNavigationMenu.Item>
   );
 }
 
