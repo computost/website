c59f3fbf6f2a81efad18acac1548432f7bda89ce
Melody Universe <melody@computost.com>
2025-05-05 09:05:20 -0700

    style services page

--- app/routes/services/services.tsx
+++ app/routes/services/services.tsx
@@ -1,3 +1,6 @@
+import type { PropsWithChildren, ReactNode } from "react";
+
+import { Card, CardTitle } from "~/components/Card";
 import { ExternalLink } from "~/components/ExternalLink";
 import { Heading } from "~/components/Heading";
 
@@ -5,8 +8,9 @@ import type { Route } from "./+types/services";
 
 export default function Services(_: Route.ComponentProps) {
   return (
-    <main>
+    <main className="mr-auto ml-auto flex max-w-6xl flex-col gap-4 p-8">
       <Heading level={1}>Services</Heading>
+
       <Heading level={2}>Standard Consulting Services</Heading>
       <p>
         Computost provides software consulting services at an hourly rate. For
@@ -16,6 +20,7 @@ export default function Services(_: Route.ComponentProps) {
         </ExternalLink>
         .
       </p>
+
       <Heading level={2}>Mission Driven Consulting Services</Heading>
       <p>
         Not all projects that provide value to the greater community have the
@@ -33,55 +38,90 @@ export default function Services(_: Route.ComponentProps) {
         based on profits are made to eventually pay off the effort spent to
         create the product.
       </p>
-      <Heading level={2}>Available Services</Heading>
-      <ul>
-        <li>New Application Development</li>
-        <li>Enhancements to Existing Applications</li>
-        <li>Code Reviews of Existing Applications</li>
-        <li>Deployment and Infrastructure Configuration</li>
-        <li>Real Time Data Integrations</li>
-        <li>Workshops</li>
-      </ul>
-      <Heading level={2}>Core Competencies</Heading>
-      <Heading level={3}>Languages</Heading>
-      <ul>
-        <li>C#</li>
-        <li>SQL</li>
-        <li>Javascript</li>
-        <li>Typescript</li>
-        <li>Powershell</li>
-        <li>Ruby</li>
-      </ul>
-      <Heading level={3}>Frameworks / Libraries</Heading>
-      <ul>
-        <li>Webpack</li>
-        <li>React</li>
-        <li>Vite</li>
-        <li>Blazor</li>
-        <li>MaterialUI</li>
-        <li>FluentUI</li>
-      </ul>
-      <Heading level={3}>Microsoft</Heading>
-      <ul>
-        <li>Power Platform</li>
-        <li>Azure</li>
-        <li>Business Central</li>
-      </ul>
-      <Heading level={3}>GitHub</Heading>
-      <ul>
-        <li>Workflows</li>
-        <li>Actions</li>
-        <li>Repo Management</li>
-      </ul>
-      <Heading level={3}>Google</Heading>
-      <ul>
-        <li>Firebase</li>
-        <li>Google Cloud Storage</li>
-      </ul>
-      <Heading level={3}>CMS Systems</Heading>
-      <ul>
-        <li>Umbraco</li>
-      </ul>
+
+      <Card>
+        <CardTitle>Available Services</CardTitle>
+        <ul className="flex flex-col gap-0.5">
+          <BigListItem>New Application Development</BigListItem>
+          <BigListItem>Enhancements to Existing Applications</BigListItem>
+          <BigListItem>Code Reviews of Existing Applications</BigListItem>
+          <BigListItem>Deployment and Infrastructure Configuration</BigListItem>
+          <BigListItem>Real Time Data Integrations</BigListItem>
+          <BigListItem>Workshops</BigListItem>
+        </ul>
+      </Card>
+
+      <Card>
+        <CardTitle>Core Competencies</CardTitle>
+        <div className="flex flex-row flex-wrap gap-8">
+          <CoreCompetenciesCategory>
+            <Heading level={3}>Languages</Heading>
+            <ul>
+              <li>C#</li>
+              <li>SQL</li>
+              <li>Javascript</li>
+              <li>Typescript</li>
+              <li>Powershell</li>
+              <li>Ruby</li>
+            </ul>
+          </CoreCompetenciesCategory>
+          <CoreCompetenciesCategory>
+            <Heading level={3}>
+              Frameworks
+              <br />/ Libraries
+            </Heading>
+            <ul>
+              <li>Webpack</li>
+              <li>React</li>
+              <li>Vite</li>
+              <li>Blazor</li>
+              <li>MaterialUI</li>
+              <li>FluentUI</li>
+            </ul>
+          </CoreCompetenciesCategory>
+          <CoreCompetenciesCategory>
+            <Heading level={3}>Microsoft</Heading>
+            <ul>
+              <li>Power Platform</li>
+              <li>Azure</li>
+              <li>Business Central</li>
+            </ul>
+          </CoreCompetenciesCategory>
+          <CoreCompetenciesCategory>
+            <Heading level={3}>GitHub</Heading>
+            <ul>
+              <li>Workflows</li>
+              <li>Actions</li>
+              <li>Repo Management</li>
+            </ul>
+          </CoreCompetenciesCategory>
+          <CoreCompetenciesCategory>
+            <Heading level={3}>Google</Heading>
+            <ul>
+              <li>Firebase</li>
+              <li>Google Cloud Storage</li>
+            </ul>
+          </CoreCompetenciesCategory>
+          <CoreCompetenciesCategory>
+            <Heading level={3}>CMS Systems</Heading>
+            <ul>
+              <li>Umbraco</li>
+            </ul>
+          </CoreCompetenciesCategory>
+        </div>
+      </Card>
     </main>
   );
+}
+
+function BigListItem({ children }: PropsWithChildren): ReactNode {
+  return (
+    <li className="rounded-lg bg-emerald-300 pt-2 pb-2 text-center text-xl dark:bg-emerald-700">
+      {children}
+    </li>
+  );
+}
+
+function CoreCompetenciesCategory({ children }: PropsWithChildren): ReactNode {
+  return <div className="flex flex-col gap-2">{children}</div>;
 }
